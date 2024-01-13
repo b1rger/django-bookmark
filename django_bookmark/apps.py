26ab08529a94a7746d7b4d5c373bdac1fca28493
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-13 15:29:16 +0100

    feat: rename project
    django-bookmarks is already taken on pypi, therefore renaming to
    django-bookmark

--- django_bookmark/apps.py
+++ django_bookmark/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoBookmarksConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
-    name = "django_bookmarks"
+    name = "django_bookmark"
